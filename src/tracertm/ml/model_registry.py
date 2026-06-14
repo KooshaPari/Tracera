f722822c54b8373702a74b71d5688bb8b5410aab
KooshaPari <kooshapari@gmail.com>
2026-06-14 16:28:32 -0700

    refactor(tracera): drop redundant model_registry.py re-export

--- src/tracertm/ml/model_registry.py
+++
@@ -1,5 +0,0 @@
-"""Backward import path for the ML model registry."""
-
-from tracertm.ml.registry import ModelEntry, ModelRegistry, ModelRegistryError
-
-__all__ = ["ModelEntry", "ModelRegistry", "ModelRegistryError"]
